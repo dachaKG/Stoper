189a51b94379936fe516e57c82159360d29f5f3c
Luka <sicluka@yahoo.com>
2018-05-08 15:23:56 +0200

    Initial commit back end servera

---
+++ StoperJava/src/main/java/com/example/StoperJava/StoperJavaApplication.java
@@ -0,0 +1,12 @@
+package com.example.StoperJava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoperJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoperJavaApplication.class, args);
+	}
+}
