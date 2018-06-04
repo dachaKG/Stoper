35cfa6dcc1bcc5ce18aa63f300541ff921a24589
daniloacim <danilo.acimovic@yahoo.com>
2018-06-04 23:45:26 +0200

    Implementation of Account tab, change password...

--- Stoper/app/src/main/java/stoper/stoper/fragments/ProfileAccountFragment.java
+++ Stoper/app/src/main/java/stoper/stoper/fragments/ProfileAccountFragment.java
@@ -1,18 +1,22 @@
 package stoper.stoper.fragments;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import stoper.stoper.R;
+import stoper.stoper.activities.ChangePasswordActivity;
+import stoper.stoper.activities.StatisticDataActivity;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class ProfileAccountFragment extends Fragment {
+public class ProfileAccountFragment extends Fragment implements View.OnClickListener {
 
 
     public ProfileAccountFragment() {
@@ -24,7 +28,55 @@ public class ProfileAccountFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile_account, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile_account, container, false);
+
+        TextView statistic_data = (TextView) view.findViewById(R.id.account_link_to_statistic_data);
+        statistic_data.setOnClickListener(this);
+
+        TextView notifications_settings = (TextView) view.findViewById(R.id.account_link_to_notifications_settings);
+        notifications_settings.setOnClickListener(this);
+
+        TextView password_change = (TextView) view.findViewById(R.id.account_link_to_password_change);
+        password_change.setOnClickListener(this);
+
+        TextView help = (TextView) view.findViewById(R.id.account_link_to_help);
+        help.setOnClickListener(this);
+
+        TextView privacy = (TextView) view.findViewById(R.id.account_link_to_privacy);
+        privacy.setOnClickListener(this);
+
+        TextView terms_of_service = (TextView) view.findViewById(R.id.account_link_to_terms_of_service_title);
+        terms_of_service.setOnClickListener(this);
+
+        TextView logout = (TextView)view.findViewById(R.id.logout);
+        logout.setOnClickListener(this);
+        return view;
     }
 
+    @Override
+    public void onClick(View view) {
+        int id = view.getId();
+        Intent intent = null;
+        switch (id){
+            case R.id.account_link_to_statistic_data:
+                intent = new Intent(getActivity(), StatisticDataActivity.class);
+                break;
+            case R.id.account_link_to_notifications_settings:
+                break;
+            case R.id.account_link_to_password_change:
+                intent = new Intent(getActivity(), ChangePasswordActivity.class);
+                break;
+            case R.id.account_link_to_help:
+                break;
+            case R.id.account_link_to_privacy:
+                break;
+            case R.id.account_link_to_terms_of_service_title:
+                break;
+            case R.id.logout:
+                break;
+        }
+        if (intent != null){
+            startActivity(intent);
+        }
+    }
 }
