ad06e667cdc2525b27a8bb6d47e04dc57cbd9e79
daniloacim <danilo.acimovic@yahoo.com>
2018-06-11 00:48:57 +0200

    Dodata ManyToMany veza izmedju user-a i ride

--- StoperJava/src/main/java/com/example/StoperJava/controller/RideController.java
+++ StoperJava/src/main/java/com/example/StoperJava/controller/RideController.java
@@ -1,6 +1,5 @@
 package com.example.StoperJava.controller;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,8 +9,11 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.StoperJava.dto.RideReservationDTO;
 import com.example.StoperJava.model.Ride;
+import com.example.StoperJava.model.User;
 import com.example.StoperJava.service.RideService;
+import com.example.StoperJava.service.UserService;
 
 @RestController
 @RequestMapping(value = "/ride")
@@ -20,6 +22,9 @@ public class RideController {
 	@Autowired
 	RideService rideService;
 	
+	@Autowired
+	UserService userService;
+	
 	@GetMapping
 	public List<Ride> findRides(){
 		System.out.println("usao u ride");
@@ -27,13 +32,19 @@ public class RideController {
 	}
 	
 	@PostMapping("/reserve")
-	public Boolean reserve(@RequestBody Long id) {
+	public Boolean reserve(@RequestBody RideReservationDTO rideReservationDTO) {
 		boolean result = false;
-		Ride ride = rideService.findOne(id).get();
+		Ride ride = rideService.findOne(rideReservationDTO.getRideId()).get();
 		if(ride != null) {
-			ride.setPassengerNumber(ride.getPassengerNumber()-1);
-			rideService.saveRide(ride);
-			result = true;
+			User passenger = userService.findByEmail(rideReservationDTO.getPassengerEmail());
+			if(passenger != null) {
+				if(ride.getPassengerNumber() > 0) {
+				ride.addPassenger(passenger);
+				ride.setPassengerNumber(ride.getPassengerNumber()-1);
+				rideService.saveRide(ride);
+				result = true;
+				}
+			}
 		}
 		return result;
 	}
@@ -42,7 +53,7 @@ public class RideController {
 	@RequestMapping(value = "/searchRides")
 	public List<Ride> searchRides(@RequestBody Ride searchRide){
 		List<Ride> rides=rideService.findAll();
-		List<Ride> ridesToSend=new ArrayList<>();
+		/*List<Ride> ridesToSend=new ArrayList<>();
 		for(Ride r : rides) {
 			if(r.getStartDestination().equals(searchRide.getStartDestination())) {
 				if(r.getEndDestination().equals(searchRide.getEndDestination())) {
@@ -53,9 +64,9 @@ public class RideController {
 					}
 				}
 			}
-		}
+		}*/
 		
-		return ridesToSend;
+		return rides;
 	}
 
 }
